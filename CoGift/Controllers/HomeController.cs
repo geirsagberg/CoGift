1cc5666f28804de4af3047efeb819bc761b86c19
Geir Sagberg <geir.sagberg@gmail.com>
2015-05-09 16:52:32 +0200

    Moved bower components to lib

--- CoGift/Controllers/HomeController.cs
+++ CoGift/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace CoGift.Controllers
 
         public IActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page!";
             return View();
         }
 
