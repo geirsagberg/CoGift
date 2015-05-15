fdfe27bb51c0179956718df491b6d693d91dfd4b
Geir Sagberg <geir.sagberg@gmail.com>
2015-05-15 21:31:34 +0200

    Added express

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="bootstrap/bootstrap.d.ts" />
 /// <reference path="jquery/jquery.d.ts" />
 /// <reference path="node/node.d.ts" />
+/// <reference path="express/express.d.ts" />
