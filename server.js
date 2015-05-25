56aed890e358876d35412cf2235e62f73f1d4e9f
Geir Sagberg <geir.sagberg@gmail.com>
2015-05-25 14:31:33 +0200

    New port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require('path');
 var app = express();
 app.use(express.static(__dirname + "/public"));
 
-var port = 3500;
+var port = process.env.PORT || 3500;
 
 var server = app.listen(port, () => {
 	console.log("Listening at http://localhost:" + port);
