1eaca85a29420dd9f96df1e50584b5a4374f30ae
Geir Sagberg <geir.sagberg@gmail.com>
2015-05-30 23:55:29 +0200

    Add email, share list utton

--- scripts/app.js
+++ scripts/app.js
@@ -58,7 +58,9 @@ var App = React.createClass({
   		if(error){
   			console.log('logIn failed: ', error);
   		}
-  	});
+  	}, {
+      scope: "email"
+    });
   },
 
   logOut() {
@@ -85,13 +87,12 @@ var App = React.createClass({
   	if (this.state.user) {
 	  	return (
 	      <div>
-          <button type='button' onClick={this.logOut}>
-            Log out
-          </button>
+          <button type='button' onClick={this.logOut}>Log out</button>
 					<form onSubmit={this.onSubmit}>
 						<input onChange={this.onChange} value={this.state.text} />
 					</form>
 					<List gifts={this.state.gifts} />
+          <button type='button' onClick={this.shareList}>Share list</button>
 				</div>
 	    );
   	} else {
