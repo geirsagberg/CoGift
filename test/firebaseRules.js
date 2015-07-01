812088459ba7a367232ca1866f689bfdf7c4edb2
Geir Sagberg <geir.sagberg@gmail.com>
2015-07-01 23:09:07 +0200

    Added functioning tests for firebase rules.

--- test/firebaseRules.js
+++ test/firebaseRules.js
@@ -1,121 +1,72 @@
 import chai, {expect} from 'chai';
 import targaryen from 'targaryen';
+import request from 'request-promise';
 chai.use(targaryen.chai);
-// import FirebaseTokenGenerator from 'firebase-token-generator';
-// import Firebase from 'firebase';
+import FirebaseTokenGenerator from 'firebase-token-generator';
 
-// var secret = process.env.FIREBASE_SECRET;
-// if (!secret) {
-//   throw new Error('process.env.FIREBASE_SECRET must be set');
-// }
+var secret = process.env.FIREBASE_SECRET;
+if (!secret) {
+  throw new Error('process.env.FIREBASE_SECRET must be set');
+}
 
-// const tokenGenerator = new FirebaseTokenGenerator();
-// const token = tokenGenerator.createToken({
-//   uid: 'test:1'
-// }, {
-//   expires: Date.now + 60,
-//   debug: true
-// });
+const tokenGenerator = new FirebaseTokenGenerator(secret);
+const token = tokenGenerator.createToken({
+  uid: 'test:1'
+}, {
+  expires: Date.now() + 60,
+  debug: true,
+  admin: true
+});
 
-beforeEach(() => {
+before(() => {
   targaryen.setFirebaseData({
-    "userMappings": {
-      "google:104306720016343733478": "-Jq_pKTwwIpsGSJi-P0X",
-      "google:106938310606776451925": "-Jse8EvBVGyhb6hOX37T",
-      "google:109454697630364037743": "-Jq_osRFI6JOxDVGOtUE",
-      "google:118093949389173065313": "-JseHDvPMawnnwHHA5Jv"
+    'userMappings': {
+      'google:1': 'user_1',
+      'google:2': 'user_2'
     },
-    "users": {
-      "-Jq_osRFI6JOxDVGOtUE": {
-        "gifts": {
-          "-Jsg1gsCtvb_Ny1N45q9": {
-            "title": "Julegave"
+    'users': {
+      'user_1': {
+        'gifts': {
+          'gift_1': {
+            'title': 'Skjerf'
           },
-          "-Jsg9fxgX6MaBfR0LH5O": {
-            "title": "Bil"
-          },
-          "-Jsg9i1Z5SE7jUE_mW6e": {
-            "title": "Sjokolade"
-          },
-          "-JsyafAdImT-q0YYoQOa": {
-            "title": "Sykkelbukse"
-          },
-          "-Jt-hvNN0h7B6_k-pRqX": {
-            "title": "Teli"
+          'gift_2': {
+            'title': 'Bil'
           }
         },
-        "userMappings": {
-          "google": "google:109454697630364037743"
+        'userMappings': {
+          'google': 'google:1'
         }
       },
-      "-Jq_pKTwwIpsGSJi-P0X": {
-        "gifts": {
-          "-Jq_pNBY8xUVV5G2kR6-": {
-            "title": "Skjera"
+      'user_2': {
+        'gifts': {
+          'gift_3': {
+            'title': 'Motorsykkel'
           },
-          "-Jqacr0X07kUEuz4dprT": {
-            "title": "Yiyi"
+          'gift_4': {
+            'title': 'PC'
           }
         },
-        "userMappings": {
-          "google": "google:104306720016343733478"
-        }
-      },
-      "-Jse8EvBVGyhb6hOX37T": {
-        "gifts": {
-          "-Jse8Ggj-vh91iplPb1n": {
-            "title": "PORSCHE"
-          }
-        },
-        "userMappings": {
-          "google": "google:106938310606776451925"
-        }
-      },
-      "-JseHDvPMawnnwHHA5Jv": {
-        "gifts": {
-          "-JseHGayUQs6zBAPjf8y": {
-            "title": "iMac"
-          },
-          "-JseHJ24XlPFXBtJt4gn": {
-            "title": "Another iMac"
-          },
-          "-JseHKEApQemjgsh37ZG": {
-            "title": "And a third iMac"
-          }
-        },
-        "userMappings": {
-          "google": "google:118093949389173065313"
+        'userMappings': {
+          'google': 'google:2'
         }
       }
     }
   });
-  targaryen.setFirebaseRules({
-    "rules": {
-      "userMappings": {
-        "$loginId": {
-          ".read": "auth !== null && auth.uid === $loginId",
-          ".write": "auth !== null && (data.val() === null || auth.uid === $loginId)"
-        }
-      },
-      "users": {
-        "$userId": {
-          ".read": "auth !== null && data.child('userMappings/' + auth.provider).val() === auth.uid",
-          ".write": "auth !== null && (data.val() === null || data.child('userMappings/' + auth.provider).val() === auth.uid)"
-        }
-      },
-      "tokens": {
-        "$tokenId": {
-          ".read": true,
-          ".write": "auth !== null && data.val() === null && newData.val() === auth.uid"
-        }
-      }
-    }
-  });
+
+  return request('https://intense-heat-531.firebaseio.com/.settings/rules.json?auth=' + token)
+    .then(JSON.parse)
+    .then(targaryen.setFirebaseRules);
 });
 
 describe('Firebase Security Rules', () => {
   it('should deny unauthorized access', () => {
-    expect(targaryen.users.unauthenticated)
-    .cannot.read.path('users/simplelogin:1');
+    expect(targaryen.users.unauthenticated).cannot.read.path('users/user_1/gifts/gift_1/title');
+  });
+  it('should allow an authenticated login to create a user', () => {
+    expect(targaryen.users.google).can.write.to.path('users/user_1');
+  });
+  it('should not allow users to write to other users', () => {
+    expect({uid: 'google:2'}).cannot.write.to.path('users/user_1');
   });
 });
