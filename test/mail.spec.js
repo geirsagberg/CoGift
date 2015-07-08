6dd073e52744c27a3a63903d374259c6d3ba4e41
Geir Sagberg <geir.sagberg@gmail.com>
2015-07-08 10:54:13 +0200

    Stub firebase

--- test/mail.spec.js
+++ test/mail.spec.js
@@ -3,27 +3,30 @@ import sinon from 'sinon';
 import Fireproof from 'fireproof';
 import chai from 'chai';
 import firebaseRef from '../common/firebase';
-
-const should = chai.should();
+chai.should();
 
 const mail = {to: 'test@test.com', subject: 'Subject', body: 'Body'};
 
 describe('Mail sender', () => {
-  var xhr, requests;
+  var xhr, requests, stub;
 
   before(() => {
     xhr = sinon.useFakeXMLHttpRequest();
     requests = [];
     xhr.onCreate = requests.push;
+    stub = sinon.stub(firebaseRef, 'child', () => {
+      return { push: () => Promise.resolve(true) };
+    });
   });
 
   after(() => {
     xhr.restore();
+    stub.restore();
   });
 
   it('sends mail and returns a promise', () => {
     firebaseRef.should.be.instanceOf(Fireproof);
-    return sendMail(mail).should.eventually.equal(true);
+    return sendMail(mail).should.eventually.be.true;
   });
 
   it('fails if no contact with Firebase', () => {
